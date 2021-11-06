ff5f1f06061bbb765979f9686589e4d2ebc13b3d
User <nahkampf-1990@mail.ru>
2021-11-06 20:28:05 +0300

    Приложение стало масштабируемым. Подключен Log4j. Добавлена запись логов в файл.

--- src/main/java/com/example/test_prodject_for_playtox_1/AddAccount.java
+++ src/main/java/com/example/test_prodject_for_playtox_1/AddAccount.java
@@ -1,12 +1,12 @@
 package com.example.test_prodject_for_playtox_1;
 import com.example.test_prodject_for_playtox_1.model.Account;
-import com.example.test_prodject_for_playtox_1.model.SomeClass;
+import com.example.test_prodject_for_playtox_1.model.TransferMoney;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class AddAccount {
     private static final List<Account> ADD_ACCOUNT = new CopyOnWriteArrayList<>();
-    private static final int COUNT_ACCOUNT = 12;
+    private static final int COUNT_ACCOUNT = 60; //число потоков должно быть четным
     private static final int START_MONEY = 10000;
 
     private static void generateAccount() {
@@ -19,7 +19,7 @@ public class AddAccount {
     public void generateThreads() {
         generateAccount();
         for(int i = 0; i < ADD_ACCOUNT.size(); i = i + 2){
-            SomeClass someClass = new SomeClass(ADD_ACCOUNT.get(i), ADD_ACCOUNT.get(i+1));
+            TransferMoney someClass = new TransferMoney(ADD_ACCOUNT.get(i), ADD_ACCOUNT.get(i+1));
             Thread thread = new Thread(someClass);
             thread.start();
         }
--- src/main/java/com/example/test_prodject_for_playtox_1/model/TransferMoney.java
+++ src/main/java/com/example/test_prodject_for_playtox_1/model/TransferMoney.java
@@ -1,32 +1,34 @@
 package com.example.test_prodject_for_playtox_1.model;
-public class SomeClass implements Runnable {
+import org.apache.log4j.Logger;
+
+public class TransferMoney implements Runnable {
+    private static final Logger log = Logger.getLogger(TransferMoney.class);
 
     private Account account1;
     private Account account2;
 
-    public SomeClass(Account account1, Account account2) {
+    public TransferMoney(Account account1, Account account2) {
         this.account1 = account1;
         this.account2 = account2;
     }
 
     public void transferMoney() {
         int transfer;
-
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 30; i++) {
             transfer = (int) (Math.random() * 3000);
             if (account1.getMoney() < transfer) {
                 int money = account1.getMoney();
                 account1.setMoney(0);
+                log.info("Со счета " + account1.getId() + " cписан остаток " + money);
                 account2.setMoney(account2.getMoney() + money);
-                System.out.println("Со счета " + account1.getId() + " cписан остаток " + money);
-                System.out.println("На счет " + account2.getId() + " зачислен остаток " + money);
+                log.info("На счет " + account2.getId() + " зачислен остаток " + money);
                 break;
             }
 
             account1.setMoney(account1.getMoney() - transfer);
-            System.out.println("Со счета " + account1.getId() + " списано " + transfer);
+            log.info("Со счета " + account1.getId() + " списано " + transfer);
             account2.setMoney(account2.getMoney() + transfer);
-            System.out.println("На счет " + account2.getId() + " зачислено " + transfer);
+            log.info("На счет " + account2.getId() + " зачислено " + transfer);
 
             try {
                 Thread.sleep((long) ((Math.random() * (2000 - 1000)) + 1000));
@@ -35,8 +37,8 @@ public class SomeClass implements Runnable {
             }
         }
 
-        System.out.println("На счете 1 " + account1.getMoney());
-        System.out.println("На счете 2 " + account2.getMoney());
+        log.info("На счете 1 " + account1.getMoney());
+        log.info("На счете 2 " + account2.getMoney());
     }
 
     @Override
